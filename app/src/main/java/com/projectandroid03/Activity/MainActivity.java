dc70455767430c7eea5cb7fbeef4645a36cfe6b5
Bearyeiz <giaphuoc.work@gmail.com>
2023-07-29 01:37:18 +0700

    ab

--- app/src/main/java/com/projectandroid03/Activity/MainActivity.java
+++ app/src/main/java/com/projectandroid03/Activity/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import android.widget.SearchView;
 
 //import com.projectandroid03.Controller.CategoryHandler;
 //import com.projectandroid03.Model.Category;
