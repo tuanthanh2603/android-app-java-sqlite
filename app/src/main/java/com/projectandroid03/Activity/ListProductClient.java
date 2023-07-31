d63e86f1b8a23b55179793773c3f1e917b0566a3
tuanthanh2603 <tuanthanhktltk21@gmail.com>
2023-07-31 17:10:25 +0700

    17:10

--- app/src/main/java/com/projectandroid03/Activity/ListProductClient.java
+++ app/src/main/java/com/projectandroid03/Activity/ListProductClient.java
@@ -3,6 +3,7 @@ package com.projectandroid03.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 import android.widget.ListView;
 
 import com.projectandroid03.Activity.Adapter.CategoryAdapterClient;
@@ -18,6 +19,7 @@ import java.util.List;
 
 public class ListProductClient extends AppCompatActivity {
     private ListView listView;
+    private GridView gridView;
     private ProductAdapterClient adapterClient;
 
     private ProductHandler productHandler;
@@ -30,11 +32,13 @@ public class ListProductClient extends AppCompatActivity {
 
 //        Category selectedCategory = (Category) getIntent().getSerializableExtra("selectedCategory");
 
-        listView = (ListView) findViewById(R.id.listViewProductClient);
+        gridView = (GridView) findViewById(R.id.girdViewProductClient);
         productHandler = new ProductHandler(this);
         List<Product> productList = productHandler.getAllProduct();
         ProductAdapterClient productAdapterClient = new ProductAdapterClient(this, productList);
-        listView.setAdapter(productAdapterClient);
+        int numberOfColumns = 2;
+        gridView.setNumColumns(numberOfColumns);
+        gridView.setAdapter(productAdapterClient);
 
 
     }
